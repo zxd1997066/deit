0f6fd69a02706003ffbbdf802d5fdfc4180b606c
zengxian <xiangdong.zeng@intel.com>
2023-06-02 15:53:21 +0800

    debug

--- engine.py
+++ engine.py
@@ -95,16 +95,16 @@ def evaluate(data_loader, model, device, args=None):
             total_time += elapsed
             total_sample += args.batch_size
 
-        acc1, acc5 = accuracy(output, target, topk=(1, 5))
+        #acc1, acc5 = accuracy(output, target, topk=(1, 5))
 
         batch_size = images.shape[0]
         metric_logger.update(loss=loss.item())
-        metric_logger.meters['acc1'].update(acc1.item(), n=batch_size)
-        metric_logger.meters['acc5'].update(acc5.item(), n=batch_size)
+        #metric_logger.meters['acc1'].update(acc1.item(), n=batch_size)
+        #metric_logger.meters['acc5'].update(acc5.item(), n=batch_size)
     # gather the stats from all processes
     metric_logger.synchronize_between_processes()
-    print('* Acc@1 {top1.global_avg:.3f} Acc@5 {top5.global_avg:.3f} loss {losses.global_avg:.3f}'
-          .format(top1=metric_logger.acc1, top5=metric_logger.acc5, losses=metric_logger.loss))
+    #print('* Acc@1 {top1.global_avg:.3f} Acc@5 {top5.global_avg:.3f} loss {losses.global_avg:.3f}'
+          #.format(top1=metric_logger.acc1, top5=metric_logger.acc5, losses=metric_logger.loss))
 
     throughput = total_sample / total_time
     latency = total_time / total_sample * 1000
